6fc643fdef0a73f504dcc373247d087a2ed51e1f
ArkaprabhaChakraborty <chakrabortyarkaprabha998@google.com>
2020-10-15 18:38:44 +0530

    final change

--- call.py
+++ call.py
@@ -6,11 +6,11 @@ import time
 t = os.getcwd()
 path = t+"\images"
 x = os.listdir(path)
-
+time = int(input("Enter Time gap in seconds: "))
 while(True):
     X = ""
     l = random.choice(x)
     X = ('%s\%s'%(path,l))
     print(X)
     ctypes.windll.user32.SystemParametersInfoW(20, 0, X , 0)
-    time.sleep(5) #this is for 5 secs.... So for 2 minutes it will be 120... and so on
+    time.sleep(time) #this is for 5 secs.... So for 2 minutes it will be 120... and so on
